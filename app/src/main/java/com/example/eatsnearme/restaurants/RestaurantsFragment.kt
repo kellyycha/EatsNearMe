6817b7508655f39c65417796017cd8d1a323271e
Kelly Cha <kcha@fb.com>
2022-06-23 11:16:54 -0700

    Put retrofit in YelpService.kt and used in RestaurantsFragment.kt

--- app/src/main/java/com/example/eatsnearme/restaurants/RestaurantsFragment.kt
+++ app/src/main/java/com/example/eatsnearme/restaurants/RestaurantsFragment.kt
@@ -8,17 +8,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.eatsnearme.MainActivity
 import com.example.eatsnearme.R
-import com.example.eatsnearme.yelp.YelpRestaurant
+import com.example.eatsnearme.yelp.API_KEY
 import com.example.eatsnearme.yelp.YelpSearchResult
 import com.example.eatsnearme.yelp.YelpService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-private const val BASE_URL = "https://api.yelp.com/v3/"
-private const val API_KEY = "s_Q0zANLiLbbp8ga5gCjJ7K-MiwmTnqTIUzX9XFnBvYvFE7iN7nMDUf7e5a6JnC9CVLPEBTfyE1zVwV-Y3zfl9IadSiIvUFyhXHnILfz7_Gt6CjscNONxY6jAwOxYnYx"
 class RestaurantsFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,10 +31,13 @@ class RestaurantsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
-        val yelpService = retrofit.create(YelpService::class.java)
-        yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York").enqueue(object :
-            Callback<YelpSearchResult> {
+        fetchRestaurants()
+    }
+
+    private fun fetchRestaurants() {
+        val yelpService = YelpService.create()
+        yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York")
+            .enqueue(object : Callback<YelpSearchResult> {
 
             override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>){
                 Log.i(MainActivity.TAG, "onResponse $response")
@@ -48,11 +47,6 @@ class RestaurantsFragment : Fragment() {
                 Log.i(MainActivity.TAG, "onFailure $t")
             }
 
-
-
         })
-
-
-
     }
 }
